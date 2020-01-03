73b8a59f633687839b39e6b0cdee0772f78f3aa3
Agnes Park <agnespark824@gmail.com>
2020-01-03 02:32:15 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -3,7 +3,10 @@ def new_hash
 end
 
 def my_hash
-  {# return a valid hash with any key/value pair of your choice}
+  { 
+    :first_name = "Agnes,"
+    :last_name = "Park"
+  }
 end
 
 def pioneer
